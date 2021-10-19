a43b019f7c9e406b357cdec353a7b55a483f0f43
curiousmango13 <70775179+curiousmango13@users.noreply.github.com>
2021-10-19 12:13:20 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/tip_calculatop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tip_calculatop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:tip_calculatop/ui/home.dart';
+
+void main()=> runApp(new MaterialApp(
+  home:BillSplitter(),
+));
